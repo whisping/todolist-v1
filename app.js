67b0495059bf21922fda2490379ab83cc8650a63
whisping <whisping@gmail.com>
2019-08-11 21:39:28 +0300

    added css, public folder

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
